d3fae103b3f90477b1771ad1175acdaccc063ab3
Dotun <dotunjolaosho@gmail.com>
2020-08-31 14:10:52 +0100

    fixes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
+import dropbox
+import requests
 from flask import Flask, request, Response, jsonify
 from dotenv import load_dotenv
 from twilio.rest import Client
-import dropbox
-import requests
 
 load_dotenv()
 app = Flask(__name__)
@@ -13,7 +13,7 @@ twilio_client = Client()
 def make_outbound_call(): 
     twilio_client.calls.create(record=True, to=os.getenv('TWILIO_TO_NUMBER'), from_=os.getenv('TWILIO_FROM_NUMBER'), 
         twiml=f"<Response><Say voice='alice'>Hello, I hope you're having a wonderful day</Say><Pause length='1'/><Say>Goodbye</Say></Response>",
-        recording_status_callback='https://64f4dea264ec.ngrok.io/recording/callback',
+        recording_status_callback=f"{os.getenv('BASE_URL')}/recording/callback",
         recording_status_callback_event='completed')
     return jsonify({ 'message': 'Call has been placed'}), 201
 
@@ -24,8 +24,7 @@ def upload_recording():
     dropbox_client = dropbox.Dropbox(os.getenv('DROPBOX_ACCESS_TOKEN'))
     upload_path = f"/twilio-recording/{recording_sid}.mp3"
     with requests.get(recording_url, stream=True) as r:
-         req_data = r.raw
-         dropbox_client.files_upload(req_data.read(), f"{upload_path}")
+         dropbox_client.files_upload(r.raw.read(), upload_path)
     return Response(), 200
 
 
